48a4e7fb93fecb557107bedbb4da19ef7253d0dc
Jesse Ruder <jesse@getexponent.com>
2019-02-27 08:53:38 -0800

    add sudos to script

--- linux/gamelift/gamelift-upload/install.sh
+++ linux/gamelift/gamelift-upload/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /local/game
-tar xvzf filebeat-6.6.1-linux-x86_64.tar.gz
+#cd /local/game
+#sudo tar xvzf filebeat-6.6.1-linux-x86_64.tar.gz
 
-rm filebeat-6.6.1-linux-x86_64/filebeat.yml
-cp filebeat.yml filebeat-6.6.1-linux-x86_64/filebeat.yml
+#sudo rm filebeat-6.6.1-linux-x86_64/filebeat.yml
+#sudo cp filebeat.yml filebeat-6.6.1-linux-x86_64/filebeat.yml
 
-cd filebeat-6.6.1-linux-x86_64
-sudo chown root filebeat.yml
-sudo ./filebeat -e -c filebeat.yml &
+#cd filebeat-6.6.1-linux-x86_64
+#sudo chown root filebeat.yml
+#sudo ./filebeat -e -c filebeat.yml &
 
 echo 'Installed Castle' > install.log
