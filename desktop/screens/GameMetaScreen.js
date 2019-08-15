be409c46d931e3aceda86ce018c0d533cd2d2838
Ben Roth <spongebucket@gmail.com>
2019-08-15 15:17:30 -0700

    desktop: cleaning up new meta screen

--- desktop/screens/GameMetaScreen.js
+++ desktop/screens/GameMetaScreen.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as Actions from '~/common/actions';
 import * as ChatUtilities from '~/common/chat-utilities';
 import * as Constants from '~/common/constants';
 
@@ -10,62 +11,61 @@ import { UserPresenceContext } from '~/contexts/UserPresenceContext';
 import ChatChannel from '~/components/chat/ChatChannel';
 import ChatMembers from '~/components/chat/ChatMembers';
 import GameMetaHeader from '~/components/gamemeta/GameMetaHeader';
+import UIHorizontalNavigation from '~/components/reusable/UIHorizontalNavigation';
 
-const STYLES_CONTAINER_BASE = `
+const STYLES_CONTAINER = css`
+  width: 100%;
+  min-width: 10
+  background: ${Constants.colors.white};
+  height: 100%;
   display: flex;
   align-items: flex-start;
   justify-content: space-between;
   flex-direction: column;
-  width: 100%;
-  min-width: 10%;
-  height: 100vh;
-  transition: 0ms ease all;
-  transition-property: transform, opacity;
-  background: ${Constants.colors.white};
-`;
-
-const STYLES_CONTAINER = css`
-  ${STYLES_CONTAINER_BASE};
-  transform: translateX(0px);
-  opacity: 1;
 `;
 
 class GameMetaScreen extends React.Component {
   state = {
-    mode: 'MESSAGES',
+    game: null,
+    mode: 'activity',
   };
-
-  constructor(props) {
-    super(props);
-    this._update(null, null);
-  }
 
   componentDidUpdate(prevProps, prevState) {
     this._update(prevProps, prevState);
   }
 
-  _update = (prevProps, prevState) => {
+  componentDidMount() {
+    this._mounted = true;
+    this._update(null, null);
+  }
+
+  componentWillUnmount() {
+    this._mounted = false;
+  }
+
+  _update = async (prevProps, prevState) => {
     const { chat, channelId } = this.props;
     if (chat) {
       chat.markChannelRead(channelId);
     }
-    if (prevProps && prevProps.channelId !== channelId) {
-      this.setState({ mode: 'MESSAGES', messageIdToEdit: null });
+    if (!prevProps || prevProps.channelId !== channelId) {
+      this._mounted && this.setState({ mode: 'activity', messageIdToEdit: null });
+      const channel = chat.channels[channelId];
+      if (channel.type === 'game' && channel.gameId) {
+        try {
+          let game = await Actions.getGameByGameId(channel.gameId);
+          this.setState({ game });
+        } catch (_) {}
+      }
     }
   };
 
-  _handleClickChannelName = () => {
-    if (this.state.mode !== 'MESSAGES') {
-      this.setState({ mode: 'MESSAGES' });
-    } else {
-      const channel = this.props.chat.channels[this.props.channelId];
-      if (channel.type === 'dm') {
-        const user = this.props.userIdToUser[channel.otherUserId];
-        if (user) {
-          this.props.navigator.navigateToUserProfile(user);
-        }
-      }
-    }
+  _getNavigationItems = () => {
+    return [{ label: 'Activity', key: 'activity' }, { label: 'Online', key: 'members' }];
+  };
+
+  _handleNavigationChange = (selectedKey) => {
+    this.setState({ mode: selectedKey });
   };
 
   _handleLeaveChannel = async () => {
@@ -73,28 +73,27 @@ class GameMetaScreen extends React.Component {
     this.props.navigator.navigateToHome();
   };
 
-  _handleShowSingleChannelMembers = () => this.setState({ mode: 'MEMBERS' });
-
   _handleOpenDirectMessage = (user) => {
     this.props.chat.openChannelForUser(user);
   };
 
   _renderContent = (channel, mode) => {
     switch (mode) {
-      case 'MEMBERS':
+      case 'members':
         return (
           <ChatMembers
             userIds={channel.subscribedUsers.map((user) => user.userId)}
             onSendMessage={this._handleOpenDirectMessage}
           />
         );
+      case 'activity':
       default:
         return <ChatChannel chat={this.props.chat} channelId={this.props.channelId} />;
     }
   };
 
   render() {
-    const { mode } = this.state;
+    const { game, mode } = this.state;
 
     if (!this.props.channelId) {
       return null;
@@ -114,14 +113,24 @@ class GameMetaScreen extends React.Component {
     return (
       <div className={STYLES_CONTAINER}>
         <GameMetaHeader
-          channel={channel}
-          mode={mode}
+          game={game}
           numChannelMembers={numChannelMembers}
           onSelectGame={this.props.navigator.navigateToGame}
           onLeaveChannel={onLeaveChannel}
-          onMembersClick={this._handleShowSingleChannelMembers}
-          onChannelClick={this._handleClickChannelName}
+          onMembersClick={() => this._handleNavigationChange('members')}
         />
+        <div
+          className={css`
+            display: block;
+            width: 100%;
+          `}>
+          <UIHorizontalNavigation
+            items={this._getNavigationItems()}
+            onChange={this._handleNavigationChange}
+            selectedKey={mode}
+            style={{ borderBottom: `1px solid #ececec`, width: '100%' }}
+          />
+        </div>
         {this._renderContent(channel, mode)}
       </div>
     );
